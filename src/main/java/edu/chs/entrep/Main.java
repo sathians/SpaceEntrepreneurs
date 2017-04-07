b14dd6543a0a94275ba8735ae7aa0cd383f4402f
niklasohlsson <nikohl@student.chalmers.se>
2017-04-07 09:39:52 +0200

    Added images of monster,spaceship, missile

--- src/main/java/edu/chs/entrep/Main.java
+++ src/main/java/edu/chs/entrep/Main.java
@@ -7,37 +7,148 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.animation.AnimationTimer;
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyEvent;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Main extends Application {
 
-    //Should be controlled via view class, this is just for prototyping
+    //Instead of a view and control class, this is handled in start()
 
-    public void start(Stage theStage) throws Exception {
-
-        theStage.getIcons().add(new Image("img/purple_monster.png"));
-        theStage.setTitle("SpaceEntrepreneurs");
-
-
-        Group root = new Group();
-        Scene theScene = new Scene(root);
-        theStage.setScene(theScene);
-
-        Canvas canvas = new Canvas(500, 600);
-        root.getChildren().add(canvas);
-
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        Image background = new Image("img/background.png");
-        Image monster = new Image("img/purple_monster.png");
-
-        gc.drawImage(background, 0, 0);
-        gc.drawImage(monster, 180, 230);
-
-        theStage.show();
-
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage theStage)
+    {
+        theStage.setTitle( "SpaceEntrepreneurs- the endless game!" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        theStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
+
+        ArrayList<String> input = new ArrayList<String>();
+
+
+        //KeyHandler - set and release
+        theScene.setOnKeyPressed(
+                new EventHandler<KeyEvent>()
+                {
+                    public void handle(KeyEvent e)
+                    {
+                        String code = e.getCode().toString();
+                        if ( !input.contains(code) )
+                            input.add( code );
+                    }
+                });
+
+        theScene.setOnKeyReleased(
+                new EventHandler<KeyEvent>()
+                {
+                    public void handle(KeyEvent e)
+                    {
+                        String code = e.getCode().toString();
+                        input.remove( code );
+                    }
+                });
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Font theFont = Font.font( "Helvetica", FontWeight.BOLD, 24 );
+        gc.setFont( theFont );
+        gc.setFill( Color.DARKRED );
+        gc.setStroke( Color.BLACK );
+        gc.setLineWidth(1);
+
+        Sprite briefcase = new Sprite();
+        briefcase.setImage("img/f0.png");
+        briefcase.setPosition(200, 480);
+
+        //Sprite missle = new Sprite();
+        //briefcase.setImage("img/missile.png", 20, 20, true);
+        ArrayList<Sprite> moneybagList = new ArrayList<Sprite>();
+
+        for (int i = 0; i < 15; i++)
+        {
+            Sprite moneybag = new Sprite();
+            moneybag.setImage("img/ufo_0.png");
+            double px = 350 * Math.random() + 50;
+            double py = 350 * Math.random() + 50;
+            moneybag.setPosition(px,py);
+            moneybagList.add( moneybag );
+        }
+
+        final Image wall_1 = new Image( "img/Firewall.png",70, 50, true, true );
+        final Image wall_2 = new Image( "img/Firewall.png",70, 50, true, true );
+        final Image wall_3 = new Image( "img/Firewall.png",70, 50, true, true );
+
+
+        LongValue lastNanoTime = new LongValue( System.nanoTime() );
+
+        IntValue score = new IntValue(0);
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                // calculate time since last update.
+                double elapsedTime = (currentNanoTime - lastNanoTime.value) / 1000000000.0;
+                lastNanoTime.value = currentNanoTime;
+
+                // game logic
+
+                briefcase.setVelocity(0,0);
+                if (input.contains("LEFT"))
+                    briefcase.addVelocity(-100,0);
+                if (input.contains("RIGHT"))
+                    briefcase.addVelocity(100,0);
+                if (input.contains("UP"))
+                    briefcase.addVelocity(0,-100);
+                if (input.contains("DOWN"))
+                    briefcase.addVelocity(0,100);
+
+                briefcase.update(elapsedTime);
+
+                // collision detection
+
+                Iterator<Sprite> moneybagIter = moneybagList.iterator();
+                while ( moneybagIter.hasNext() )
+                {
+                    Sprite moneybag = moneybagIter.next();
+                    if ( briefcase.intersects(moneybag) )
+                    {
+                        moneybagIter.remove();
+                        score.value++;
+                    }
+                }
+
+                // render
+
+                gc.clearRect(0, 0, 512,512);
+                briefcase.render( gc );
+
+                for (Sprite moneybag : moneybagList )
+                    moneybag.render( gc );
+
+                String pointsText = "Cash: $" + (100 * score.value);
+                gc.fillText( pointsText, 360, 36 );
+                gc.strokeText( pointsText, 360, 36 );
+                gc.drawImage( wall_1, 50, 400 );
+                gc.drawImage( wall_2, 200, 400 );
+                gc.drawImage( wall_3, 350, 400 );
+            }
+        }.start();
+
+        theStage.show();
     }
 }
