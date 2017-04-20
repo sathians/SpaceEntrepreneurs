d7b038417e0dccced750ecb1b12ab837555d86c2
VersaceVersaceVersace <ahmadyan@student.chalmers.se>
2017-04-20 10:40:21 +0200

    new character

--- src/main/java/edu/chs/entrep/model/Character.java
+++ src/main/java/edu/chs/entrep/model/Character.java
@@ -1,7 +1,27 @@
 package edu.chs.entrep.model;
 
+import javafx.scene.image.Image;
+
 /**
  * Created by josefinesvegborn on 2017-04-03.
  */
 public class Character {
+
+    private Image Character;
+    private double posX;
+    private double posY;
+    private double velX;
+    private double velY;
+
+    private double width;
+    private double height;
+
+    public Character(){
+        posX = 0;
+        posY= 0;
+
+        Character = new Image( "img/spaceship_a1.png");
+
+    }
+
 }
