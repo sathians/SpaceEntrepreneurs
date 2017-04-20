3554ea24f95b2c91a11cafa05f9a7a306cc75e68
sathians <sathian@student.chalmers.se>
2017-04-20 10:49:38 +0200

    Started with missile

--- src/main/java/edu/chs/entrep/model/Missile.java
+++ src/main/java/edu/chs/entrep/model/Missile.java
@@ -1,7 +1,31 @@
 package edu.chs.entrep.model;
 
+import javafx.scene.image.Image;
+
+import java.lang.*;
+
 /**
  * Created by josefinesvegborn on 2017-04-03.
  */
 public class Missile {
+
+    private Image Missile;
+    private double posX;
+    private double posY;
+    private double velY;
+
+    private double width;
+    private double height;
+
+    public Missile (){
+
+        posX = Character.posX;
+        velY = 0;
+
+        Missile = new Image  ("img/Tesla_missile_0.png");
+
+
+    }
 }
+
+
--- src/main/java/edu/chs/entrep/model/Monster.java
+++ src/main/java/edu/chs/entrep/model/Monster.java
@@ -49,5 +49,4 @@ public class Monster {
         return monsterList;
     }
 
-
 }
