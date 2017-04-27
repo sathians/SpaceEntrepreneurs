afa07ec33e94cb728b21b15191749a6ead34c261
jsvegborn <josefinesvegborn@gmail.com>
2017-04-27 14:36:25 +0200

    foundation for testing

---
+++ src/main/java/edu/chs/entrep/model/Menu.java
@@ -0,0 +1,10 @@
+package edu.chs.entrep.model;
+
+import javafx.scene.Scene;
+
+/**
+ * Created by josefinesvegborn on 2017-04-27.
+ */
+public class Menu {
+    private String welcomeText = "SpaceEntrepreneurs";
+}
