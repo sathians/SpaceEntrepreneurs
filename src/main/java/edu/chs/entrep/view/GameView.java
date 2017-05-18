c254b76cc9fd37abb9c3f868d2439f61afa664d0
jsvegborn <josefinesvegborn@gmail.com>
2017-05-18 10:18:38 +0200

    Changed the Main into several view-control classes

--- src/main/java/edu/chs/entrep/view/GameView.java
+++ src/main/java/edu/chs/entrep/view/GameView.java
@@ -1,15 +1,265 @@
 package edu.chs.entrep.view;
 
+import edu.chs.entrep.model.Highscore;
+import edu.chs.entrep.model.Monster;
+import edu.chs.entrep.model.Player;
+import edu.chs.entrep.model.SpaceEntrepreneurs;
+import edu.chs.entrep.service.Images;
+import edu.chs.entrep.service.Sound;
+import javafx.animation.AnimationTimer;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by josefinesvegborn on 2017-04-03.
  */
 public class GameView {
+    private Player player;
+    private Stage theStage;
+    private int level;
+    private boolean nextLevel;
+    private ArrayList<String> input;
+    private Canvas canvas;
+    private GraphicsContext gc;
+    private Images images;
+    File highscoreFile;
+    Highscore highscore;
+    HighscoreView highscoreView;
 
 
+    //Could we reomve highscore from this file??
+
+    public GameView(Stage theStage, Player player) {
+        this.player = player;
+        this.theStage = theStage;
+        this.level = 1;
+        this.nextLevel = false;
+        this.input = new ArrayList<String>();
+        this.canvas = new Canvas(512, 527);
+        this.gc = canvas.getGraphicsContext2D();
+        this.images = new Images();
+        this.highscoreFile = new File("src/main/resources/txt/highscore");
+        this.highscore = new Highscore(highscoreFile);
+
+        createComponents();
+    }
+
+    public void createComponents() {
+
+        Group root = new Group();
+        root.getChildren().add(canvas);
+        Scene gameScene = new Scene(root);
+
+        Font theFont = Font.font("Futura", FontWeight.LIGHT, 16);
+        gc.setFont(theFont);
+        gc.setFill(Color.WHITE);
+        gc.setLineWidth(1);
+        theStage.setScene(gameScene);
+
+        //create event handler for buttons
+        gameScene.setOnKeyPressed(
+                e -> {
+                    String code = e.getCode().toString();
+                    if (!input.contains(code))
+                        input.add(code);
+                });
+
+        gameScene.setOnKeyReleased(
+                e -> {
+                    String code = e.getCode().toString();
+                    input.remove(code);
+                });
+    }
+
+    public void showGameStage() {
+        theStage.show();
+    }
+
+    public void startGame() {
+
+        new Sound().bgdSound();
+
+        new AnimationTimer() {
+            long lastNanoTime = System.nanoTime();      //Check if this can be removed
+            SpaceEntrepreneurs spaceEntrepreneurs = new SpaceEntrepreneurs(player, level, highscore);
+
+            /*Gets all images from the Images class*/
+            Image background_img = images.getBackgroundImage(level);
+            Image cover_img = images.getCoverImage(level);
+            Image monster1_img = images.getMonsterImage(level);
+
+            Image spaceship_img = images.getSpaceshipImage();
+            Image missile_img = images.getMissileImage();
+
+            Image gameOver_img = images.getGameOverImage();
+            Image clearedLevel_img = images.getClearedLevelImage();
+            Image life_img = images.getLifeImage();
+
+            //handles counting blinking image of spaceship when getting hit
+            double time = 0;
+            boolean blink = false;
+            boolean hitImageIsOn = false;
+
+            public void handle(long currentNanoTime) {
+
+                // calculate time since last update.
+                double elapsedTime = (currentNanoTime - lastNanoTime) / 1000000000.0;
+                lastNanoTime = currentNanoTime;
+
+                // game logic
+                spaceEntrepreneurs.spaceship.setVelocity(0, 0);          //How do we sett this in logic in spaceEntrepreneurs instead?
+                if (input.contains("LEFT")) {
+                    spaceEntrepreneurs.left();
+                }
+
+                if (input.contains("RIGHT")) {
+                    spaceEntrepreneurs.right();
+                }
+
+                if (input.contains("SPACE")) {           //även tidigare även && !missile.isOnScreen()
+                    spaceEntrepreneurs.shoot();
+                }
+
+                // Min tanke här är att med ett visst tids-inervall så skall monstrena hoppa ner ett steg närmare rymdskeppet.
+                // if (elapsedTime > 1 && elapsedTime < 2 || elapsedTime > 10 && elapsedTime < 11) {
+                //    for(Sprite monster: monsterList)
+                //        monster.addPosition(0, 50);
+                //}
+
+                //Updates spaceship, missile and monster position
+
+                spaceEntrepreneurs.spaceship.update(elapsedTime);
+                spaceEntrepreneurs.missile.update(elapsedTime);
+                spaceEntrepreneurs.monsterMissile.update(elapsedTime);
+
+                for (Monster monster : spaceEntrepreneurs.getMonsterList())
+                    monster.update(elapsedTime);
+
+
+                // collision detection - returns true if the spaceship is hit
+
+                if (spaceEntrepreneurs.collisionCheck()) {
+                    spaceship_img = images.getHitSpaceshipImage();
+                    hitImageIsOn = true;
+                    blink = true;
+                }
+
+                if (blink == true) {
+                    time = time + elapsedTime;
+                    if (((int) (time * 100) % 10) == 0 && time < 0.5) {
+                        if (hitImageIsOn) {
+                            spaceship_img = images.getSpaceshipImage();
+                            hitImageIsOn = false;
+                        } else {
+                            spaceship_img = images.getHitSpaceshipImage();
+                            hitImageIsOn = true;
+                        }
+                    } else if (time > 0.5) {
+                        spaceship_img = images.getSpaceshipImage();
+                        blink = false;
+                        hitImageIsOn = false;
+                        time = 0;
+                    }
+                }
+
+                spaceEntrepreneurs.moveMonster();
+                spaceEntrepreneurs.monsterShoot();
+
+                gc.clearRect(0, 0, 512, 512);
+                gc.drawImage(background_img, 0, 0);
+                gc.drawImage(spaceship_img, spaceEntrepreneurs.spaceship.getPositionX(), spaceEntrepreneurs.spaceship.getPositionY());
+                switch (spaceEntrepreneurs.level) {
+                    case 1:
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover1.getPositionX(), spaceEntrepreneurs.cover1.getPositionY());
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover2.getPositionX(), spaceEntrepreneurs.cover2.getPositionY());
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover3.getPositionX(), spaceEntrepreneurs.cover3.getPositionY());
+                        break;
+
+                    case 2:
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover1.getPositionX(), spaceEntrepreneurs.cover1.getPositionY());
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover3.getPositionX(), spaceEntrepreneurs.cover3.getPositionY());
+                        break;
+
+                    case 3:
+                        gc.drawImage(cover_img, spaceEntrepreneurs.cover2.getPositionX(), spaceEntrepreneurs.cover2.getPositionY());
+                        break;
+                }
+
+                if (spaceEntrepreneurs.missile.isOnScreen())
+                    gc.drawImage(missile_img, spaceEntrepreneurs.missile.getPositionX(), spaceEntrepreneurs.missile.getPositionY());
+
+                if (spaceEntrepreneurs.monsterMissile.isOnScreen())
+                    gc.drawImage(missile_img, spaceEntrepreneurs.monsterMissile.getPositionX(), spaceEntrepreneurs.monsterMissile.getPositionY());
+
+                for (Monster monster : spaceEntrepreneurs.getMonsterList()) {
+                    gc.drawImage(monster1_img, monster.getPositionX(), monster.getPositionY());
+                    //monster.render( gc );
+                }
+
+                String pointsText = "CASH $" + (player.getScore());
+                gc.fillText(pointsText, 395, 36);
+                //gc.strokeText(pointsText, 360, 36);
+
+                String levelText = "LEVEL " + (level);
+                gc.fillText(levelText, 240, 36);
+
+
+                String lifeText = ("LIFE  ");
+                for (int i = 1; i <= spaceEntrepreneurs.player.getLife(); i++) {
+                    gc.drawImage(life_img, 30 + 25 * i, 20);
+                }
+                gc.fillText(lifeText, 20, 36);
+                //gc.strokeText(lifeText, 20, 36);
+
+                if (spaceEntrepreneurs.monsterCheck()) {
+                    level = level + 1;
+                    gc.drawImage(clearedLevel_img, 0, 0);
+                    stop();
+                    new Timer().schedule(
+                            new TimerTask() {
+                                @Override
+                                public void run() {
+                                    start();
+                                }
+                            },
+                            2000
+                    );
+
+                    spaceEntrepreneurs = new SpaceEntrepreneurs(player, level, highscore);
+                    //Changes images to level3
+                    background_img = images.getBackgroundImage(level);
+                    cover_img = images.getCoverImage(level);
+                    monster1_img = images.getMonsterImage(level);
+                }
+
+                if (spaceEntrepreneurs.gameOverCheck()) {
+                    spaceEntrepreneurs.checkHighscore();
+
+                    gc.drawImage(gameOver_img, 0, 0); //HOW COULD THIS BE SHOWN?
+
+                    stop();
+
+                    highscoreView = new HighscoreView(theStage, player);
+                    highscoreView.showHighscoreStage();
+                }
+            }
+
+        }.start();
+    }
 }
+
+
+
--- src/main/java/edu/chs/entrep/view/MenuView.java
+++ src/main/java/edu/chs/entrep/view/MenuView.java
@@ -1,7 +1,72 @@
 package edu.chs.entrep.view;
 
+import edu.chs.entrep.model.Highscore;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import sun.rmi.rmic.newrmic.Main;
+
 /**
  * Created by josefinesvegborn on 2017-04-03.
  */
 public class MenuView {
+
+    private Stage theStage;
+    private HighscoreView highscoreView;
+    private NameView nameView;
+
+    public MenuView(Stage theStage) {
+        this.theStage = theStage;
+        createComponents();
+    }
+
+    public void createComponents() {
+
+
+        String buttonStyle = "-fx-font-family: Futura; -fx-font-size: 18px; -fx-text-fill: #FFFFFF; -fx-background-color: rgb(0,0,0,0.0); -fx-border-weight: 0.5px; -fx-border-color: white; -fx-border-radius: 2px;";
+        String buttonPressedStyle = "-fx-font-family: Futura; -fx-font-size: 18px; -fx-text-fill: #cccccc; -fx-background-color: rgb(0,0,0,0); -fx-border-weight: 0.5px; -fx-border-color: #cccccc; -fx-border-radius: 2px";
+
+        Label label1 = new Label("Space Entrepreneurs");
+        Button highscoreButton = new Button("HIGHSCORES");
+        Button startButton = new Button("Start");
+        VBox layout1 = new VBox(20);
+        layout1.setAlignment(Pos.CENTER);
+        startButton.setStyle(buttonStyle);
+        highscoreButton.setStyle(buttonStyle);
+        layout1.setStyle("-fx-background-image: url(img/background.png);");
+        layout1.getChildren().addAll(label1, startButton, highscoreButton);
+        Scene startScene = new Scene(layout1, 512, 512);
+        theStage.setScene(startScene);
+
+        //Button Handlers
+        startButton.setOnAction(e -> {
+            System.out.print("hej");
+            startGame();
+        });
+
+        highscoreButton.setOnAction(e -> {
+            showHighscores();
+        });
+    }
+
+    public void showMenuStage() {
+        this.theStage.show();
+    }
+
+    public boolean startGame() {
+        nameView = new NameView(theStage);
+        nameView.showNameStage();
+        return true;
+    }
+
+    public boolean showHighscores() {
+        highscoreView = new HighscoreView(theStage);
+        highscoreView.showHighscoreStage();
+        return true;
+    }
+
 }
+
